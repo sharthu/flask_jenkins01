571b3483dd2e268e3fab94ec2954109b56e3a68a
Shar Thu <mr.sharthu@gmail.com>
2025-04-13 15:45:41 +0630

    upload successful app

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({'message': 'Hello from the Backend!'})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
